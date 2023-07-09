ec816520903daa1769bca3dd934f372629cb70ec
InEmEcI <iikar@bk.ru>
2023-07-09 15:09:10 +0300

    добавил краш-тест

--- backend/app.js
+++ backend/app.js
@@ -9,20 +9,29 @@ const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 
-app.use(requestLogger); // подключаем логгер запросов
-
 const router = require('./routes/index');
 
 const { PORT = 3000 } = process.env;
-app.use(cors());
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
 
+app.use(cors());
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+app.use(requestLogger);
+
 app.use(express.json());
 
 app.use(router);
+
+app.use(errorLogger);
 
 app.use(errors());
 app.use(errorHandler);
